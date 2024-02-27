f43df017b4f28ebe8c5d14383dba4e8bd69353fd
Amar Badiger <132143642+Amarbadiger@users.noreply.github.com>
2024-02-27 13:00:42 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  "mongodb+srv://amarbadiger:C2jrykPzYiQE6Gh0@cluster0.fh5tewx.mongodb.net/Todo-App"
+  ""
 );
 
 const todoSchema = mongoose.Schema({
